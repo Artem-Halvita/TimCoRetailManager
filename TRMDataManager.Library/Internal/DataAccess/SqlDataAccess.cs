580fbf73c677fcf5f8da8fac704e47dea0bba04f
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-10-22 11:52:27 +0300

    Add Transactions
    Add transactions to SQL Data Access and used them for the SalesData call.

--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace TRMDataManager.Library.Internal.DataAccess
 {
-    internal class SqlDataAccess
+    internal class SqlDataAccess : IDisposable
     {
         string GetConnectionString(string name)
         {
@@ -40,5 +40,55 @@ namespace TRMDataManager.Library.Internal.DataAccess
                     commandType: CommandType.StoredProcedure);
             }
         }
+
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+
+        public void StartTransaction(string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+
+            _transaction = _connection.BeginTransaction();
+        }
+
+        public List<T> LoadDataInTransaction<T, U>(string storedParameter, U parameters)
+        {
+            List<T> rows = _connection.Query<T>(storedParameter, parameters,
+                    commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+            return rows;
+        }
+
+        public void SaveDataInTransaction<T>(string storedParameter, T parameters)
+        {
+            _connection.Execute(storedParameter, parameters,
+                    commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+
+        public void CommitTransaction()
+        {
+            _transaction?.Commit();
+            _connection?.Close();
+        }
+
+        public void RollbackTransaction()
+        {
+            _transaction?.Rollback();
+            _connection?.Close();
+        }
+
+        public void Dispose()
+        {
+            CommitTransaction();
+        }
+
+        // Open connect/start transaction method
+        // load using transaction
+        // save using transaction
+        // Close connection/stop transaction method
+        // Dispose
     }
 }
