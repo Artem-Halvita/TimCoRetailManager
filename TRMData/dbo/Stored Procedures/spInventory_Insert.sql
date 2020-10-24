108e036d6b9759088a9067badacfa8770437cae5
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-10-24 19:57:43 +0300

    Add Admin Endpoint Data
    Adds the data for administrative endpoints. Does not yet secure the endpoints beyond authorization.

---
+++ TRMData/dbo/Stored Procedures/spInventory_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spInventory_Insert]
+	@ProductId int,
+	@Quantity int,
+	@PurchasePrice decimal,
+	@PurchaseDate datetime2
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.Inventory(ProductId, Quantity, PurchasePrice, PurchaseDate)
+	VALUES (@ProductId, @Quantity, @PurchasePrice, @PurchaseDate);
+END
--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -84,10 +84,15 @@ namespace TRMDataManager.Library.DataAccess
                     throw;
                 }
             }
+        }
 
+        public List<SaleReportModel> GetSaleReport()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
 
-            
+            var output = sql.LoadData<SaleReportModel, dynamic>("dbo.spSale_SaleReport", new { }, "TRMData");
 
+            return output;
         }
     }
 }
