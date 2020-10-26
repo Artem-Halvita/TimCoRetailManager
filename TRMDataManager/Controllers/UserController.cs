62b00cc3ca07a1ee0365a601cd6072564129ccdf
Artem <35197523+Artem-Halvita@users.noreply.github.com>
2020-10-26 16:16:31 +0300

    Remove unless attribute

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -10,14 +10,12 @@ using TRMDataManager.Library.Internal.Models;
 namespace TRMDataManager.Controllers
 {
     [Authorize]
-    [RoutePrefix("api/User")]
     public class UserController : ApiController
     {
         [HttpGet]
         public UserModel GetById()
         {
             string userId = RequestContext.Principal.Identity.GetUserId();
-
             UserData data = new UserData();
 
             return data.GetUserById(userId).First();
